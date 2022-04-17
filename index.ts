986ba961a8fcda98561b2b30c34d0f7c2d8c28dc
Abass Adamo <adamoabasstope@yahoo.com>
2022-04-17 16:32:40 +0200

    iniital push

---
+++ index.ts
@@ -0,0 +1,15 @@
+import {createServer} from '@marblejs/http'
+import {IO} from 'fp-ts/lib/IO';
+import { listener } from './http.listener';
+
+const server = createServer({
+    port:5000,
+    hostname: '127.0.0.1',
+    listener
+})
+
+const main: IO<void> = async()=>{
+    await (await server)();
+}
+
+main();
